373542c253e69c6122c06a821ff366939434d676
KarthikaVishnu <104002134+KarthikaVishnu@users.noreply.github.com>
2022-04-19 16:40:39 +0530

    Add files via upload

---
+++ Axisbank.java
@@ -0,0 +1,17 @@
+package week3.day2.assignment;
+
+public class Axisbank extends BankInfo{
+	
+	public void deposit() {
+		
+		System.out.println("Rs.5000 has been deposited in your Account");
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Axisbank ax=new Axisbank();
+		ax.deposit();
+	}
+
+}
